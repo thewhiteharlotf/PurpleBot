864e99efa9f58b61fdd8f85252509114e2ce5978
Purple <scryptt@gmail.com>
2021-01-28 19:03:32 -0300

    Figlet refeito

--- userbot/modules/figlet.py
+++ userbot/modules/figlet.py
@@ -3,21 +3,21 @@
 # Licensed under the Raphielscape Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
 #
-# Port to UserBot by @MoveAngel
 
 import pyfiglet
+from emoji import get_emoji_regexp
 
 from userbot import CMD_HELP
 from userbot.events import register
 
 
-@register(outgoing=True, pattern=r"^\.figlet(?: |$)(.*)")
-async def figlet(fg):
-    if fg.fwd_from:
+@register(outgoing=True, pattern=r"^\.figlet (\w+) (.+)")
+async def figlet(event):
+    if event.fwd_from:
         return
-    CMD_FIG = {
+    style_list = {
         "slant": "slant",
-        "3D": "3-d",
+        "3d": "3-d",
         "5line": "5lineoblique",
         "alpha": "alphabet",
         "banner": "banner3-D",
@@ -30,33 +30,27 @@ async def figlet(fg):
         "bulb": "bulbhead",
         "digi": "digital",
     }
-    input_str = fg.pattern_match.group(1)
-    if "." in input_str:
-        text, cmd = input_str.split(".", maxsplit=1)
-    elif input_str is not None:
-        cmd = None
-        text = input_str
-    else:
-        await fg.edit("`Please add some text to figlet`")
-        return
-    if cmd is not None:
-        try:
-            font = CMD_FIG[cmd]
-        except KeyError:
-            await fg.edit("`Invalid selected font.`")
-            return
-        result = pyfiglet.figlet_format(text, font=font)
-    else:
-        result = pyfiglet.figlet_format(text)
-    await fg.respond("‌‌‎`{}`".format(result))
-    await fg.delete()
+    style = event.pattern_match.group(1)
+    text = event.pattern_match.group(2)
+    try:
+        font = style_list[style]
+    except KeyError:
+        return await event.edit(
+            "**Estilo inválido selecionado, consulte **`.help figlet`**.**")
+    result = pyfiglet.figlet_format(deEmojify(text), font=font)
+    await event.respond(f"‌‌‎`{result}`")
+    await event.delete()
 
 
-CMD_HELP.update(
-    {
-        "figlet": ".figlet"
-        "\nUso: Enhance ur text to strip line with anvil."
-        "\n\nExample: `.figlet <Text Style>`"
-        "\nSTYLE LIST: `slant`, `3D`, `5line`, `alpha`, `banner`, `doh`, `iso`, `letter`, `allig`, `dotm`, `bubble`, `bulb`, `digi`"
-    }
-)
+def deEmojify(inputString):
+    """ Removido emojis e outros caracteres não seguros da string """
+    return get_emoji_regexp().sub("", inputString)
+
+
+CMD_HELP.update({
+    "figlet":
+    ">`.figlet`"
+    "\nUso: Estiliza o texto."
+    "\n\nExemplo: `.figlet <estilo> <texto>`"
+    "\nEstilos Disponíveis: `slant`, `3d`, `5line`, `alpha`, `banner`, `doh`, `iso`, `letter`, `allig`, `dotm`, `bubble`, `bulb`, `digi`"
+})
